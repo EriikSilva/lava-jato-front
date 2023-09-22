4a986daa64fcd6604c77b0d96ab030ecf86532ca
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-09-22 16:06:31 -0400

    feat: incluindo dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
+//PRIMENG
+import { DividerModule } from 'primeng/divider';
+import { TabViewModule } from 'primeng/tabview';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DividerModule,
+    TabViewModule,
+    ButtonModule,
+    PasswordModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
