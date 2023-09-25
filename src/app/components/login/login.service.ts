8a14dbe9582acd6edba101adc9ae981b54f2121c
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-09-25 16:21:00 -0400

    feat:add funcao de login

--- src/app/components/login/login.service.ts
+++ src/app/components/login/login.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/app/environments/environment.development';
-
+import { UserLoginDTO  } from './DTO/userDTO';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,11 @@ export class LoginService {
 
   private readonly apiUrl = environment.apiUrl
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  userLogin(user:UserLoginDTO):Observable<UserLoginDTO>{
+    return this.http.post<UserLoginDTO>(this.apiUrl + "/login", user)
+  }
+
+
 }
