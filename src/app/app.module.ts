c1cb4fd1f9a7a162f51201757bc633e7e1c1d202
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-09-25 15:49:03 -0400

    feat:adicionando libs de form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 
+//COMPONENTS
+import { FrankensteinComponent } from './components/frankenstein/frankenstein.component';
+import { ServicosComponent } from './components/servicos/servicos.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //PRIMENG
@@ -18,9 +25,9 @@ import { PasswordModule } from 'primeng/password';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ToolbarModule } from 'primeng/toolbar';
 import { SidebarModule } from 'primeng/sidebar';
-import { FrankensteinComponent } from './components/frankenstein/frankenstein.component';
-import { ServicosComponent } from './components/servicos/servicos.component';
-import { ClientesComponent } from './components/clientes/clientes.component';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,6 +40,9 @@ import { ClientesComponent } from './components/clientes/clientes.component';
   ],
   imports: [
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     DividerModule,
@@ -41,7 +51,10 @@ import { ClientesComponent } from './components/clientes/clientes.component';
     PasswordModule,
     InputTextModule,
     ToolbarModule,
-    SidebarModule
+    SidebarModule,
+    MessageModule,
+    MessagesModule,
+    ToastModule
     
   ],
   providers: [],
