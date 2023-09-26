c5eb394d9c0cd213c52a8e6f76d762743323ce77
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-09-26 16:00:12 -0400

    feat:table em clientes

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientesService } from './clientes.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ClientRegisterDTO } from './DTO/clientesDTO';
 
 @Component({
   selector: 'app-clientes',
@@ -20,6 +22,15 @@ export class ClientesComponent implements OnInit{
       this.getClients();
   }
 
+  clientRegisterForm = new FormGroup({
+    nm_cliente: new FormControl('', Validators.required),
+    cpf_cnpj:   new FormControl('', Validators.required),
+    cep:        new FormControl('', Validators.required),
+    bairro:     new FormControl('', Validators.required),
+    nr_casa:    new FormControl('', Validators.required)
+  })
+
+ /**********************REQUESTS GET, POST, EDIT, DELETE ***************************/
   getClients(){
     this.clientsService.getClients()
     .subscribe({
@@ -33,7 +44,46 @@ export class ClientesComponent implements OnInit{
     })
   }
 
-  // DIALOG
+  saveClient(){
+    const formValue = this.clientRegisterForm.value
+
+    const nm_cliente = formValue.nm_cliente || "";
+    const cpf_cnpj   = formValue.cpf_cnpj || "";
+    const cep        = formValue.cep || "";
+    const bairro     = formValue.bairro || "";
+    const nr_casa    = formValue.nr_casa || "";
+
+    const bodyRegistro:ClientRegisterDTO = {
+      nm_cliente,
+      cpf_cnpj,
+      cep,
+      bairro,
+      nr_casa
+    }
+    
+    this.clientsService.postClients(bodyRegistro)
+    .subscribe({
+      next: (res:any) => {
+        console.log(res)
+      },
+       error: (res:any) => {
+        console.log('res', res)
+      }
+    })
+  }
+
+
+  editClient(cliente:any){
+    console.log('asçkdjaslkdj')
+  }
+
+  deleteClient(cliente:any){
+    console.log('aksdjklasjd')
+  }
+
+
+
+  /*******************DIALOG********************/
   openNew() {
     this.clientDialog = true;
   }
@@ -42,8 +92,8 @@ export class ClientesComponent implements OnInit{
     this.clientDialog = false;
   }
 
-  saveClient(){
-    console.log('aslçkdjasldkj')
-  }
+ 
+
+
 
 }
--- src/app/components/clientes/clientes.service.ts
+++ src/app/components/clientes/clientes.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/app/environments/environment.development';
+import { ClientRegisterDTO } from './DTO/clientesDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -16,4 +17,7 @@ export class ClientesService {
     return this.http.get<any>(this.apiUrl + '/cliente')
   }
 
+  postClients(client:ClientRegisterDTO):Observable<ClientRegisterDTO>{
+    return this.http.post<ClientRegisterDTO>(this.apiUrl + '/cliente', client)
+  }
 }
