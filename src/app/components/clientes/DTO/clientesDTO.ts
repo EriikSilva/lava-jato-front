e7e816d9eefd2a2dfaca86015be3b80eb4e5d4fe
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-09-26 17:01:53 -0400

    Clientes Dtos

--- src/app/components/clientes/DTO/clientesDTO.ts
+++ src/app/components/clientes/DTO/clientesDTO.ts
@@ -1,7 +1,38 @@
-export interface ClientRegisterDTO{
-    nm_cliente: string,
-    cpf_cnpj:   string,
-    cep:        string,
-    bairro:     string,
+export interface ClientRegisterDTO {
+    nm_cliente: string;
+    cpf_cnpj:   string;
+    cep:        string;
+    bairro:     string;
     nr_casa:    string;
+}
+
+export interface ClienteDeleteDTO{
+    cd_cliente: number;
+}
+export interface ClienteEditDTO {
+    cd_cliente: number;
+    nm_cliente: string;
+    cpf_cnpj:   string;
+    cep:        string;
+    bairro:     string;
+    nr_casa:    string;
+    status:     string;
+}
+export interface ClienteGetDTO {
+    cd_cliente:        number;
+    nm_cliente:        string;
+    cpf_cnpj:          string;
+    dt_cadastro:       string;
+    dt_update:         string;
+    cep:               string;
+    bairro:            string;
+    nr_casa:           string;
+    cd_usuario:        null;
+    status:            string;
+    veiculos_clientes: VeiculosCliente[];
+}
+
+export interface VeiculosCliente {
+    placa:  string;
+    modelo: string;
 }
