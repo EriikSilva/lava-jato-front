6d43398105c295b6a110a080c5cdc5729ba30f5f
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-10-02 16:27:23 -0400

    atributos componente

--- src/app/components/clientes/dialogs/car-details/car-details.component.ts
+++ src/app/components/clientes/dialogs/car-details/car-details.component.ts
@@ -1,49 +1,70 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ClienteGetDTO, VeiculosCliente } from '../../DTO/clientesDTO';
+import { CarrosService } from '../../carros.service';
+import { GetTypeCarDTO } from '../../DTO/carrosDTO';
 
 @Component({
   selector: 'app-car-details',
   templateUrl: './car-details.component.html',
-  styleUrls: ['./car-details.component.scss']
+  styleUrls: ['./car-details.component.scss'],
 })
 export class CarDetailsComponent {
 
-  @Input() carClientDialog: boolean = false
-  @Input() position:        string = 'center';
+  constructor(private carrosService:CarrosService){}
+
+  @Input() carClientDialog: boolean = false;
+  @Input() position: string = 'center';
+  @Input() cd_cliente: any;
+  carsType:any
 
   @Output() dialogClosed = new EventEmitter<void>();
-  
-  clientsVehicles?: Array<VeiculosCliente> | any
-  @Input() cd_cliente: any;
 
 
+  clientsVehicles?: Array<VeiculosCliente> | any;
+  newCarDialog: boolean = false;
 
-  carClientModal(position: string, cliente:ClienteGetDTO){
-    const { veiculos_clientes } = cliente
+  carClientModal(position: string, cliente: ClienteGetDTO) {
+    const { veiculos_clientes, cd_cliente } = cliente;
 
+    this.cd_cliente     = cd_cliente
     this.position        = position;
-    this.carClientDialog = true
-    this.clientsVehicles = veiculos_clientes
+    this.carClientDialog = true;
+    this.clientsVehicles = veiculos_clientes;
   }
 
-  
+  openDialogNewCar() {
+    this.newCarDialog = true;
+
+    this.carrosService.getTypeCar()
+    .subscribe({
+      next:(res: { data: GetTypeCarDTO}) =>{
+        const { data } = res
+        this.carsType = data
+      }, error:(error) => {
+        console.log('error', error)
+      }
+    })
+  }
+
+  onDialogClosed() {
+    this.newCarDialog = false;
+  }
+
   onRowEditInit(veiculos_clientes: VeiculosCliente) {
-    this.clientsVehicles[veiculos_clientes.modelo as string] = { ...veiculos_clientes };
+    this.clientsVehicles[veiculos_clientes.modelo as string] = {
+      ...veiculos_clientes,
+    };
   }
 
-  onRowEditSave(veiculos_clientes: VeiculosCliente) {
-    
-  }
+  onRowEditSave(veiculos_clientes: VeiculosCliente) {}
 
   onRowEditCancel(veiculos_clientes: VeiculosCliente, index: number) {
     // this.veiculos_clientes[index] = this.veiculos_clientes[veiculos_clientes.placa as string];
     // delete this.clonedProducts[product.id as string];
-    console.log('veiculos_clientes',veiculos_clientes)
+    console.log('veiculos_clientes', veiculos_clientes);
   }
-  
 
   closeDialog() {
     this.dialogClosed.emit();
   }
-
 }
