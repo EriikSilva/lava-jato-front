1b7fd9342234cb6b2e8572c6ac1633f0d1101a36
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-10-05 11:26:30 -0400

    DTO em testes unitarios

--- src/app/components/clientes/clientes.service.spec.ts
+++ src/app/components/clientes/clientes.service.spec.ts
@@ -1,10 +1,9 @@
-import { ClientRegisterDTO } from './DTO/clientesDTO';
+import { ClientEditDTO, ClientRegisterDTO, ClienteDeleteDTO } from './DTO/clientesDTO';
 import { TestBed } from '@angular/core/testing';
 
 import { ClientesService } from './clientes.service';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { of } from 'rxjs';
 import { environment } from 'src/app/environments/environment.development';
 
 fdescribe('ClientesService', () => {
@@ -13,24 +12,6 @@ fdescribe('ClientesService', () => {
 
   const apiUrl = environment.apiUrl
 
-  const httpSub = {
-    get: () => of( {
-      "cd_cliente": 27,
-      "nm_cliente": "teste de validation",
-      "cpf_cnpj": "12312038129038",
-      "dt_cadastro": "2023-10-05T13:53:33.000Z",
-      "dt_update": "2023-10-05T13:53:33.000Z",
-      "cep": "69074815",
-      "bairro": "teste",
-      "nr_casa": "10",
-      "cd_usuario": null,
-      "status": "A",
-      "telefone1": "27317391827",
-      "telefone2": "17239081273",
-      "veiculos_clientes": []
-  })
-  }
-
   it('Chamar Get', () => {
     const spy = spyOn(http, 'get').and.callThrough()
     service.getClients();
@@ -38,7 +19,7 @@ fdescribe('ClientesService', () => {
   })
 
   it('Chamar Post', () => {
-    const dummyClientPost = {
+    const dummyClientPost:ClientRegisterDTO = {
       nm_cliente: 'Erik Felipe',
       cpf_cnpj:   '00000000000',     
       cep:        '00000000',       
@@ -54,7 +35,7 @@ fdescribe('ClientesService', () => {
 
 
   it('Chamar Put', () => {
-    const dummyClientPut = {
+    const dummyClientPut: ClientEditDTO= {
       nm_cliente: 'Erik Felipe',
       cpf_cnpj:   '00000000000',     
       cep:        '00000000',       
@@ -71,7 +52,7 @@ fdescribe('ClientesService', () => {
   })
 
   it('Chamar Delete', () => {
-    const dummyClientDelete = {
+    const dummyClientDelete:ClienteDeleteDTO = {
       cd_cliente: 1
     }
 
