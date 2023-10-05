c6dfb620f9d4e94f6041fbafe06d7dddb9eecb71
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-10-05 16:34:24 -0400

    apagando

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -31,11 +31,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('lava-jato-front');
   });
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('lava-jato-front app is running!');
-  // });
 });
