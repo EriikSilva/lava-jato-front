96f7fec0bccb3baaf8957d52cc3e6fcb86ec9f5e
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-10-09 17:01:35 -0400

    mudando para atendimento

--- src/app/components/atendimento/atendimento.service.ts
+++ src/app/components/atendimento/atendimento.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/app/environments/environment.development';
-import { FinalizarServicoDTO } from './DTO/servicosDTO';
+import { FinalizarServicoDTO } from './DTO/atendimentoDTO';
 
 @Injectable({
   providedIn: 'root'
