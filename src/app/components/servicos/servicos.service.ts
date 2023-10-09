dfd0256ef0969b4539429aad5bd836b3cb60c509
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-10-09 10:45:03 -0400

    adicionando rotas

--- src/app/components/servicos/servicos.service.ts
+++ src/app/components/servicos/servicos.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/app/environments/environment.development';
+import { FinalizarServicoDTO } from './DTO/servicosDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,14 @@ export class ServicosService {
 
   atendimentosAgendamento(cd_cliente:string):Observable<any>{
     return this.http.get(`${this.apiUrl}/atendimentos/agendamento/${cd_cliente}`)
+  }
+  
+  servicosFinalizados():Observable<any>{
+    return this.http.get(`${this.apiUrl}/atendimentos/servicosFinalizados`)
+  }
+
+  finalizarServico(body:FinalizarServicoDTO):Observable<FinalizarServicoDTO>{
+    return this.http.post<FinalizarServicoDTO>(`${this.apiUrl}/atendimentos/finalizarServico`, body)
   }
 
 }
