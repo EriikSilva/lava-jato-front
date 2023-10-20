54ec9f243c7a539abbf8bb8819c1261467114cbe
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-10-20 16:26:53 -0400

    rota de cep

--- src/app/services/cep.service.ts
+++ src/app/services/cep.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CepService {
 
+  private apiUrl = environment.apiUrl
   constructor(private http: HttpClient) { }
 
   getEnderecoByCep(cep: string): Observable<any> {
-    return this.http.get(`https://viacep.com.br/ws/${cep}/json/`);
+    return this.http.get(`${this.apiUrl}/cliente/buscarCep/${cep}`)
   }
 }
