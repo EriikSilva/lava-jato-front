1df00766d60c3be6f744db08247eeb308bdff69e
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-10-21 15:19:58 -0400

    limpando dados

--- src/app/components/atendimento/atendimento.component.ts
+++ src/app/components/atendimento/atendimento.component.ts
@@ -23,14 +23,10 @@ export class AtendimentoComponent implements OnInit, OnDestroy {
   @ViewChild('VisualizarServicoComponent') VisualizarServicoComponent: VisualizarServicoComponent | undefined;
 
 
-  items: any[] = [];
-  clientDetails: any;
-  selectedItem: any;
-  dadosAtendimento: any;
-  cd_cliente: any;
-  contatoCliente: any;
+  items: any[] = []; //clientes
+  nrAtendimento:number = 0;
 
-  atendimentos: any;
+  atendimentos: AtendimentoDTO[] = [];
 
   atendimentoDialog: boolean = false;
   finalizarAtendimentoDialog: boolean = false;
@@ -38,30 +34,10 @@ export class AtendimentoComponent implements OnInit, OnDestroy {
 
   private destroy$: Subject<void> = new Subject<void>();
 
-  suggestions: any[] = [];
-
   constructor(
     private clientsService: ClientesService,
     private atendimentoService: AtendimentoService,
-    private confirmationService: ConfirmationService,
-    private messageService: MessageService,
-    private ServicosService: ServicosService
   ) {}
-
-  // search(event: AutoCompleteCompleteEvent) {
-  //   let filtered: any[] = [];
-  //   let query = event.query;
-
-  //   for (let i = 0; i < (this.items as any[]).length; i++) {
-  //     let client = (this.items as any[])[i];
-  //     if (client.nm_cliente.toLowerCase().indexOf(query.toLowerCase()) == 0) {
-  //         this.clientDetails = client
-  //       filtered.push(client.nm_cliente + ' - ' + client.bairro);
-  //     }
-  //   }
-
-  //   this.suggestions = filtered;
-  // }
 
   getAtendimentos() {
     this.atendimentoService.getAtendimentosAgendamentos().subscribe({
@@ -82,66 +58,6 @@ export class AtendimentoComponent implements OnInit, OnDestroy {
     });
   }
 
-  finalizarAtendimento(atendimento: any) {
-    // this.finalizarAtendimentoDialog = true
-    const nr_atendimento = atendimento.dadosAtendimento.nr_atendimento;
-    const nr_servico =
-      atendimento.dadosAtendimento.dadosServico[0].nr_servico_atendimento;
-
-    this.confirmationService.confirm({
-      message: 'Deseja Finalizar Esse Atendimento?',
-      header: 'Atenção',
-      icon: 'pi pi-exclamation-triangle',
-      acceptLabel: 'Sim',
-      rejectLabel: 'Não',
-      accept: () => {
-        this.finalizarServico(nr_atendimento, nr_servico);
-        // this.finalizarAtendimento2(nr_atendimento, nr_servico)
-      },
-    });
-  }
-
-  finalizarServico(nr_atendimento_p: number, nr_servico_p: number) {
-    this.ServicosService.finalizarServico(
-      nr_atendimento_p,
-      nr_servico_p
-    ).subscribe({
-      next: (res: any) => {
-        const { message } = res;
-        this.messageService.add({
-          severity: 'success',
-          summary: 'Sucesso',
-          detail: message,
-        });
-
-        this.finalizarAtendimento2(nr_atendimento_p, nr_servico_p);
-      },
-      error: (res: any) => {
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Erro ao Finalizar Serviço',
-          detail: res.error.error,
-        });
-      },
-    });
-  }
-
-  finalizarAtendimento2(nr_atendimento: number, nr_servico: number) {
-    this.atendimentoService
-      .finalizarAtendimento(nr_atendimento, nr_servico)
-      .subscribe({
-        next: (res: any) => {
-          console.log(res);
-        },
-        error: (res: any) => {
-          this.messageService.add({
-            severity: 'error',
-            summary: 'Erro ao Finalizar Atendimento',
-            detail: res.error.error,
-          });
-        },
-      });
-  }
   newAtendimento() {
     this.atendimentoDialog = true;
   }
@@ -149,17 +65,12 @@ export class AtendimentoComponent implements OnInit, OnDestroy {
   visualizarServicos(atendimento: AtendimentoDTO) {
     const { cd_cliente, nr_atendimento } = atendimento
     this.visualizarServicoDialog = true
-    this.cd_cliente =  cd_cliente
-    this.VisualizarServicoComponent?.getServicosByClient(this.cd_cliente, nr_atendimento)
+    this.VisualizarServicoComponent?.getServicosByClient(String(cd_cliente), nr_atendimento)
   }
   applyFilterGlobal($event: any, stringVal: any) {
     this.dt!.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
   }
-  limparPesquisa() {
-    this.dadosAtendimento = '';
-    this.clientDetails = '';
-    this.selectedItem = '';
-  }
+
 
   onDialogClosed() {
     this.atendimentoDialog = false;
