945dcfdeb0f2bfdedd9c01db622f6a4b1042ae85
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-10-21 12:34:21 -0400

    correção

--- src/app/components/atendimento/atendimento.component.spec.ts
+++ src/app/components/atendimento/atendimento.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ServicosComponent } from './servicos.component';
+import { ServicosComponent } from '../servicos/servicos.component';
 import { PrimengModule } from 'src/app/primeng/primeng.module';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppModule } from 'src/app/app.module';
