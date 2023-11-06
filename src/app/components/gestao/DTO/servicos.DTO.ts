135939f8ed2aa5d6ed1b3da36f21766130a3dfd2
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-11-06 15:13:10 -0400

    incluindo DTO de put

--- src/app/components/gestao/DTO/servicos.DTO.ts
+++ src/app/components/gestao/DTO/servicos.DTO.ts
@@ -13,3 +13,10 @@ export interface PostTipoPagamento {
     descricao:    string;
     qtd_parcelas: number;
 }
+
+export interface PutTipoPagamento {
+    cd_pagamento:      number;
+    descricao:         string;
+    qtd_parcelas:      number;
+}
+
