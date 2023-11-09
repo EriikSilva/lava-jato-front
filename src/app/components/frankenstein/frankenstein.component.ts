b1b2aea4db1477fc193f2543af8204e0a4d10747
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-11-09 11:14:30 -0400

    frankesntein

--- src/app/components/frankenstein/frankenstein.component.ts
+++ src/app/components/frankenstein/frankenstein.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-frankenstein',
@@ -13,6 +14,52 @@ export class FrankensteinComponent {
 
   openDialog(){
     this.abrirDialog = true
+  }
+
+
+  questionForm:any
+  tipos_pregunta = [
+    {
+      label: "Texto abierto",
+      value: 1
+    },
+    {
+      label: "Selección simple",
+      value: 2
+    },
+    {
+      label: "Selección múltiple",
+      value: 3
+    },
+    {
+      label: "Verdadero y Falso",
+      value: 4
+    },
+    {
+      label: "Rango",
+      value: 5
+    }
+  ];
+
+  constructor(  ) {
+    this.createForm();
+  }
+
+  ngOnInit(): void {}
+
+  createForm() {
+    this.questionForm = new FormGroup({
+      pregunta: new FormControl(''),
+      tipo_de_pregunta: new FormControl(null)
+    })
+
+    this.questionForm?.valueChanges.subscribe((e:any) => {
+      this.questionForm?.setValue(e, { emitEvent: false });
+    });
+  }
+
+  onSubmit() {
+    console.log(this.questionForm?.value);
   }
 
 
