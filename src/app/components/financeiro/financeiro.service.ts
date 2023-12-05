e1bc9cafcbd4f61471ed5645fc0ca317421faf8a
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-12-05 14:50:55 -0400

    rota de listar movimentações

---
+++ src/app/components/financeiro/financeiro.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/app/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FinanceiroService {
+  private readonly apiUrl = environment.apiUrl
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  getListaMovimetacoes(){
+    return this.http.get(`${this.apiUrl}/financeiro/listarMovimentacoes`)
+  }
+}
