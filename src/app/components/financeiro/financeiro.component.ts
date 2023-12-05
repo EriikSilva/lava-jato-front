791ed4d00aabf268d13624b2fbbc971a3726d63a
Erik Felipe <61124602+EriikSilva@users.noreply.github.com>
2023-12-05 15:32:35 -0400

    financeiro parte 1

--- src/app/components/financeiro/financeiro.component.ts
+++ src/app/components/financeiro/financeiro.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FinanceiroService } from './financeiro.service';
 
 @Component({
   selector: 'app-financeiro',
   templateUrl: './financeiro.component.html',
-  styleUrls: ['./financeiro.component.scss']
+  styleUrls: ['./financeiro.component.scss'],
+  providers: [FinanceiroService]
 })
-export class FinanceiroComponent {
+export class FinanceiroComponent implements OnInit{
+ 
+  movimentacoes: [] = [];
+  isLoading: boolean = false;
+  requisicaoCompleta: boolean = false
+
+  ngOnInit(): void {
+    this.listarMovimentacoes();
+  }
+    
+  constructor(
+    private financeiroService:FinanceiroService
+  ){}
+  
+
+  listarMovimentacoes(){
+    this.financeiroService.getListaMovimetacoes()
+    .subscribe({
+      next:(res:any) => {
+        this.isLoading = true;
+        const { data } = res
+
+        setTimeout(() => {
+          this.isLoading = false
+          this.requisicaoCompleta = true
+        }, 1000)
+
+        this.movimentacoes = data
+      }, error:(res) => {
+        this.isLoading = false;
+        console.log(res)
+      }
+    })
+  }
+
+ 
+
 
 }
